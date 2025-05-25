856b582f4a4b5663a8121780b7b98ae172839d35
tylerjusfly <tylerjusfly1@gmail.com>
2025-05-25 15:31:49 +0100

    added dice project

--- src/data/ProjectsData.ts
+++ src/data/ProjectsData.ts
@@ -1,40 +1,46 @@
 const ProjectsData = [
   {
-    title: 'Sellit Frontend',
-    Edited: 'December 2024',
-    image: './img/sellitui.png',
+    title: "Sellit Frontend",
+    Edited: "December 2024",
+    image: "./img/sellitui.png",
     description: `A UI for a digital marketplace project`,
-    url: 'https://sellit-pied.vercel.app/',
+    url: "https://sellit-pied.vercel.app/"
   },
   {
-    title: 'Sellit Backend Api',
-    Edited: 'December 2024',
-    image: './img/api.png',
+    title: "Sellit Backend Api",
+    Edited: "December 2024",
+    image: "./img/api.png",
     description: `An APi for a digital marketplace project`,
-    url: 'https://github.com/tylerjusfly/sellit_backend',
+    url: "https://github.com/tylerjusfly/sellit_backend"
   },
   {
-    title: 'Deda Survey',
-    Edited: 'December 2022',
-    image: './img/dedasurvey.png',
-    description: 'Collecting data for customer sastisfaction with React',
-    url: 'https://harmonious-pasca-27fd9c.netlify.app/rrrer',
+    title: "Deda Survey",
+    Edited: "December 2022",
+    image: "./img/dedasurvey.png",
+    description: "Collecting data for customer sastisfaction with React",
+    url: "https://harmonious-pasca-27fd9c.netlify.app/rrrer"
   },
   {
-    title: 'Food Library',
-    Edited: 'August 2022',
-    image: './img/foodazure.PNG',
-    description: 'A simple application where you can post food view foods and all',
-    url: 'https://github.com/tylerjusfly/azure-devto-hackathon',
+    title: "Food Library",
+    Edited: "August 2022",
+    image: "./img/foodazure.PNG",
+    description: "A simple application where you can post food view foods and all",
+    url: "https://github.com/tylerjusfly/azure-devto-hackathon"
   },
   {
-    title: 'Code Funhouse',
-    Edited: 'November 2024',
-    image: './img/codefunhouse.png',
-    description:
-      '',
-    url: 'https://www.codefunhouse.com/',
+    title: "Code Funhouse",
+    Edited: "November 2024",
+    image: "./img/codefunhouse.png",
+    description: "",
+    url: "https://www.codefunhouse.com/"
   },
+  {
+    title: "Dice thrower",
+    Edited: "November 2023",
+    image: "./img/dieprj.png",
+    description: "",
+    url: "https://imaginative-cuchufli-976291.netlify.app/"
+  }
 ];
 
 export { ProjectsData };
